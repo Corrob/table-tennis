37edc041e0abf9c0a2c1865810f60767354dcb6a
Corrob <corrob7@hotmail.com>
2013-05-02 15:06:24 -0700

    Add Box2d test code in GameScreen

--- TableTenis/src/com/me/tabletennis/GameScreen.java
+++ TableTenis/src/com/me/tabletennis/GameScreen.java
@@ -1,20 +1,42 @@
 package com.me.tabletennis;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.math.Vector2;
 
 public class GameScreen implements Screen{
+	static final float WORLD_TO_BOX = 0.01f;
+	static final float BOX_TO_WORLD = 100f;
+	
 	TableTenis game;
+	OrthographicCamera camera;
+	World world;
+	Box2DDebugRenderer debugRenderer;
 	
 	GameScreen(TableTenis game) {
 		this.game = game;
+		
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 10, 5);
+		world = new World(new Vector2(0, -10), true);
+		debugRenderer = new Box2DDebugRenderer();
+
+		createBall();
+		createGround();
 	}
 	
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(1, 1, 0, 1);
+		// Clear screen with black
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		// Render the world and go to the next step
+		debugRenderer.render(world, camera.combined);
+		
+		world.step(delta, 8, 3);
 	}
 
 	@Override
@@ -39,5 +61,55 @@ public class GameScreen implements Screen{
 
 	@Override
 	public void dispose() {
+	}
+	
+	// Code from box2d tutorial
+	private void createBall() {
+		// First we create a body definition
+		BodyDef bodyDef = new BodyDef();
+		// We set our body to dynamic, for something like ground which doesnt move we would set it to StaticBody
+		bodyDef.type = BodyType.DynamicBody;
+		// Set our body's starting position in the world
+		bodyDef.position.set(5, 5);
+
+		// Create our body in the world using our body definition
+		Body body = world.createBody(bodyDef);
+
+		// Create a circle shape and set its radius to 6
+		CircleShape circle = new CircleShape();
+		circle.setRadius(0.1f);
+
+		// Create a fixture definition to apply our shape to
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = circle;
+		fixtureDef.density = 0.5f; 
+		fixtureDef.friction = 0.4f;
+		fixtureDef.restitution = 0.8f; // Make it bounce a little bit
+
+		// Create our fixture and attach it to the body
+		body.createFixture(fixtureDef);
+		
+		circle.dispose();
+	}
+	
+	// More code for box2d tutorial
+	private void createGround() {
+		// Create our body definition
+		BodyDef groundBodyDef =new BodyDef();  
+		// Set its world position
+		groundBodyDef.position.set(new Vector2(5, 1));  
+
+		// Create a body from the defintion and add it to the world
+		Body groundBody = world.createBody(groundBodyDef);  
+
+		// Create a polygon shape
+		PolygonShape groundBox = new PolygonShape();  
+		// Set the polygon shape as a box which is twice the size of our view port and 20 high
+		// (setAsBox takes half-width and half-height as arguments)
+		groundBox.setAsBox(4f, 0.25f);
+		// Create a fixture from our polygon shape and add it to our ground body  
+		groundBody.createFixture(groundBox, 0.0f); 
+		// Clean up after ourselves
+		groundBox.dispose();
 	}
 }
