3db5468d105506af57b4153450c053740d59a830
Corrob <corrob7@hotmail.com>
2013-05-02 14:26:40 -0700

    Change resolution to 800x480 and use GL20

--- TableTenis-desktop/src/com/me/tabletennis/Main.java
+++ TableTenis-desktop/src/com/me/tabletennis/Main.java
@@ -6,10 +6,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "TableTenis";
-		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.title = "Table Tennis";
+		cfg.useGL20 = true;
+		cfg.width = 800;
+		cfg.height = 480;
 		
 		new LwjglApplication(new TableTenis(), cfg);
 	}
