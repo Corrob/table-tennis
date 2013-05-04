4b298e1d44d8cf31177f986b5517bf15c637c3e3
Corrob <corrob7@hotmail.com>
2013-05-03 17:02:31 -0700

    Project maintainence - Remove html project - had trouble loading and ran slow - Change package name to com.funprog.tabletennis for fun programming

---
+++ TableTennis/src/com/funprog/tabletennis/Ball.java
@@ -0,0 +1,53 @@
+package com.funprog.tabletennis;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+
+/*
+* The Ball class maintains a sprite and body for a ball
+* and provides information and methods for a table tennis
+* ball.
+*/
+public class Ball {
+	Body body;
+	/*
+	* The constructor that requires the world in which to
+	* add the ball and the position where it starts.
+	*
+	* @param world The world to add the ball
+	* @param position The position where the ball starts
+	*/
+	public Ball(World world, Vector2 position) {
+		// Create the body definition
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.type = BodyType.DynamicBody;
+		bodyDef.position.set(position.x, position.y);
+		
+		// Add the body to the world
+		body = world.createBody(bodyDef);
+		
+		// Define the shape and characteristics of the ball
+		CircleShape circle = new CircleShape();
+		circle.setRadius(0.1f);
+		
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = circle;
+		fixtureDef.density = 0.5f;
+		fixtureDef.friction = 0.4f;
+		fixtureDef.restitution = 0.8f;
+		
+		body.createFixture(fixtureDef);
+		
+		// Clean up
+		circle.dispose();
+	}
+	
+	public void gravity() {
+		body.applyForce(new Vector2(0, -0.5f), body.getWorldCenter(), true);
+	}
+}
---
+++ TableTennis/src/com/funprog/tabletennis/Table.java
@@ -0,0 +1,39 @@
+package com.funprog.tabletennis;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
+/*
+* The table class creates a static body in the world
+* at the defined location.
+*/
+public class Table {
+	/*
+	* Construct that will add the table to the world with the specified
+	* dimensions.
+	*
+	* @param world The world where the table is to be added
+	* @param center Then center of the table
+	* @param width The width of the table
+	* @param height The height of the table
+	*/
+	public Table(World world, Vector2 center, float width, float height) {
+		// Create the body definition of the table
+		BodyDef tableDef =new BodyDef();
+		tableDef.position.set(center);
+		
+		// Add the body to the world
+		Body tableBody = world.createBody(tableDef);
+		
+		// Define the shape of the table
+		PolygonShape tableBox = new PolygonShape();
+		tableBox.setAsBox(width / 2, height / 2);
+		tableBody.createFixture(tableBox, 0.0f);
+		
+		// Clean up
+		tableBox.dispose();
+	}
+}
