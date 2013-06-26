b57c9851f085bca2bbb2b74a77393f059b8086bc
Corrob <corrob7@hotmail.com>
2013-06-25 23:00:56 -0700

    Add Javadoc to ComputerPaddle

--- TableTennis/src/com/funprog/tabletennis/ComputerPaddle.java
+++ TableTennis/src/com/funprog/tabletennis/ComputerPaddle.java
@@ -11,6 +11,14 @@ import com.badlogic.gdx.physics.box2d.World;
 public class ComputerPaddle extends Paddle {
 	private float xGoal;
 	
+	/**
+	 * Creates a computer paddle with the given conditions.
+	 * 
+	 * @param world the world in which the paddle resides
+	 * @param position the starting position of the paddle
+	 * @param texture the texture (image) of the paddle
+	 * @param xGoal the x position that the paddle strives for
+	 */
 	public ComputerPaddle(World world, Vector2 position, Texture texture, float xGoal) {
 		super(world, position, texture);
 		
@@ -19,6 +27,13 @@ public class ComputerPaddle extends Paddle {
 		setRotation(-0.2f);
 	}
 	
+	/**
+	 * The paddle defends its position by guessing where the ball will go.
+	 * 
+	 * @param ball the ball that the paddle will try to hit
+	 */
+	// TODO(Corrob): Add capabilities of predicting if the ball will hit the table and decide
+	// to hit the ball or let it pass if it will go out.
 	public void defendPosition(Ball ball) {
 		Body body = ball.getBody();
 	
@@ -30,6 +45,18 @@ public class ComputerPaddle extends Paddle {
 		}
 	}
 	
+	/**
+	 * Returns the y value in which the line intersects with x = value. Utilizes the
+	 * equation y - y1 = m (x - x1) to solve for the unknown. 
+	 * (x1, y1) is a point on the line.
+	 * m is the slope of the line.
+	 * 
+	 * @param x the x = value where the line intersects
+	 * @param x1 the x value of a point on the line
+	 * @param y1 the y value of a point on the line
+	 * @param m the slope of the line
+	 * @return the y value of the intersection with x = value
+	 */
 	private float intersectWithXEquals(float x, float x1, float y1, float m) {
 		return m * (x - x1) + y1;
 	}
